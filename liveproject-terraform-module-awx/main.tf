ade8ed7c0abd81c0ffa9fcee4529456606d89825
Michael Haller <mike.haller@gmail.com>
2021-02-06 18:14:32 -0500

    updates for initial tf module

--- liveproject-terraform-module-awx/main.tf
+++ liveproject-terraform-module-awx/main.tf
@@ -51,9 +51,47 @@ resource "aws_vpc" "awx-public" {
     }
 }
 
+resource "aws_subnet" "awx-subnet" {
+  vpc_id            = aws_vpc.awx-public.id
+  cidr_block        = "10.1.0.0/24"
+
+  tags = {
+    Name = "awx subnet"
+    Cluster = "none"
+    Project = "Mastering Compliance with Ansible, Terraform, and OpenSCAP"
+    Environment = "dev"
+    Creator = "terraform"
+    Expires = "Never"
+    Service = "awx"
+    Management = "terraform"
+  }
+}
+
+resource "aws_network_interface" "awx-interface" {
+  subnet_id   = aws_subnet.awx-subnet.id
+  private_ips = ["10.1.0.1"]
+
+  tags = {
+    Name = "awx primary interface"
+    Cluster = "none"
+    Project = "Mastering Compliance with Ansible, Terraform, and OpenSCAP"
+    Environment = "dev"
+    Creator = "terraform"
+    Expires = "Never"
+    Service = "awx"
+    Management = "terraform"
+  }
+}
+
 resource "aws_instance" "awx" {
     ami = data.aws_ami.RHEL.id
     instance_type = "t2.medium"
+
+    network_interface {
+        network_interface_id = aws_network_interface.awx-interface.id
+        device_index         = 0
+    }
+
     tags = {
         Name = "awx"
         Cluster = "none"
