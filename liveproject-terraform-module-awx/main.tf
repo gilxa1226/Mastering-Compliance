c77c795f93c9364790095aa8934588619072360c
Michael Haller <mike.haller@gmail.com>
2021-02-07 10:58:10 -0500

    updates from testing.

--- liveproject-terraform-module-awx/main.tf
+++ liveproject-terraform-module-awx/main.tf
@@ -27,8 +27,8 @@ data "aws_ami" "RHEL" {
     }
 }
 
-resource "aws_vpc" "awx-private" {
-    cidr_block  = "10.0.0.0/28"
+resource "aws_vpc" "awx" {
+    cidr_block  = "10.1.0.0/16"
     tags = {
         Name = "awx-private"
         Cluster = "none"
@@ -41,26 +41,28 @@ resource "aws_vpc" "awx-private" {
     }
 }
 
-resource "aws_vpc" "awx-public" {
-    cidr_block  = "10.1.0.0/28"
-    tags = {
-        Name = "awx-public"
-        Cluster = "none"
-        Project = "Mastering Compliance with Ansible, Terraform, and OpenSCAP"
-        Environment = "dev"
-        Creator = "terraform"
-        Expires = "Never"
-        Service = "public subnet"
-        Management = "terraform"
-    }
-}
-
-resource "aws_subnet" "awx-subnet" {
-  vpc_id            = aws_vpc.awx-public.id
-  cidr_block        = "10.1.0.0/28"
+resource "aws_subnet" "awx-public-subnet" {
+  vpc_id            = aws_vpc.awx.id
+  cidr_block        = "10.1.2.0/24"
 
   tags = {
-    Name = "awx subnet"
+    Name = "awx public subnet"
+    Cluster = "none"
+    Project = "Mastering Compliance with Ansible, Terraform, and OpenSCAP"
+    Environment = "dev"
+    Creator = "terraform"
+    Expires = "Never"
+    Service = "awx"
+    Management = "terraform"
+  }
+}
+
+resource "aws_subnet" "awx-private-subnet" {
+  vpc_id            = aws_vpc.awx.id
+  cidr_block        = "10.1.3.0/24"
+
+  tags = {
+    Name = "awx private subnet"
     Cluster = "none"
     Project = "Mastering Compliance with Ansible, Terraform, and OpenSCAP"
     Environment = "dev"
@@ -72,7 +74,7 @@ resource "aws_subnet" "awx-subnet" {
 }
 
 resource "aws_network_interface" "awx-interface" {
-  subnet_id   = aws_subnet.awx-subnet.id
+  subnet_id   = aws_subnet.awx-public-subnet.id
   private_ips = ["10.1.0.10"]
 
   tags = {
@@ -85,6 +87,22 @@ resource "aws_network_interface" "awx-interface" {
     Service = "awx"
     Management = "terraform"
   }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "liveproject-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC7BY/XLvHkUfRWnwGKFJGQPYIOFNMVH/v132mJBTmyi2kDFWdFKZujZ5xiE0pG9IAP6GtTqaHfenCQNTuYPcR3x1XJkXJXbUOw4srxIE8rId3/orK3uGN2e9imd4DVCSxNq/tXrzG3gUAwxVqNG4WHcDz0PsOztm6uB/zbzu8d1Mp8uF1rrBHN3rz0YWQHNkoaH3WZSYkXnSxp04C5foY++L2qWODXKOaqfxn3dZk3+Qc7n0ANb5o5vHAzskODptorzOpfp2U9LZxeAhOhRkP8IAWt5wvSwHfIWADv9G1x/iHUMp1HvEKInr2Mv9H1sVLnggoxKBIj6UcjxIxacEJxFpfh7uwb0MnUOSANGkgPMuqwSBtRfb8Hi1QLOplgsRDMowTcv8IDSzMBVxmy0oBMNUvxss6Tvg4uYoxeBtThnpF544QWPZYrUvjn1NdHUmJEYp28+9TK7Y1iaG4u5BMT8BIU3IcwSweLBKXxlii6tmJf3jnyUhUeTqzgYf4BgO0= ec2-user@ip-172-31-42-246.us-east-2.compute.internal"
+
+  tags = {
+        Name = "awx ssh keypair"
+        Cluster = "none"
+        Project = "Mastering Compliance with Ansible, Terraform, and OpenSCAP"
+        Environment = "dev"
+        Creator = "terraform"
+        Expires = "Never"
+        Service = "awx"
+        Management = "terraform"
+    }
 }
 
 resource "aws_instance" "awx" {
@@ -106,5 +124,21 @@ resource "aws_instance" "awx" {
         Service = "awx"
         Management = "terraform"
     }
-
 }
+
+resource "aws_eip" "awx-ip" {
+  instance = aws_instance.awx.id
+  vpc      = true
+
+  tags = {
+        Name = "awx elastic IP"
+        Cluster = "none"
+        Project = "Mastering Compliance with Ansible, Terraform, and OpenSCAP"
+        Environment = "dev"
+        Creator = "terraform"
+        Expires = "Never"
+        Service = "awx"
+        Management = "terraform"
+    }
+}
+
